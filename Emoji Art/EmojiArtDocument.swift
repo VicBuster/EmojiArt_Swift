0fe6ad8fb0316ee01d2deedb4a5fd8dee4086c12
Cephei.DZ <Buster2491@gmail.com>
2024-05-23 16:13:44 +0800

    Init more

--- Emoji Art/EmojiArtDocument.swift
+++ Emoji Art/EmojiArtDocument.swift
@@ -7,8 +7,41 @@
 
 import SwiftUI
 
+// ViewModel
 class EmojiArtDocument: ObservableObject {
     
+    typealias Emoji = EmojiArt.Emoji
     private var emojiArt = EmojiArt()
     
+    var emojis: [Emoji] {
+        emojiArt.emojis
+    }
+    
+    var backgorund: URL? {
+        emojiArt.background
+    }
+    
+    // MARK: - Intent(s)
+    func setBackground(_ url: URL?) {
+        emojiArt.background = url
+    }
+    
+    func addEmoji(_ emoji: String, at position: Emoji.Position, size: CGFloat) {
+        emojiArt.addEmoji(emoji, at: position, size: Int(size))
+    }
+    
+    
+}
+
+extension EmojiArt.Emoji {
+    var font: Font {
+        Font.system(size: CGFloat(size))
+    }
+}
+
+extension EmojiArt.Emoji.Position {
+    // in cannot be used as an identifier here
+    func `in`(_ geometry: GeometryProxy) -> CGPoint {
+        let center = geometry.frame(in: .local).center
+    }
 }
--- Emoji Art/EmojiArtDocumentView.swift
+++ Emoji Art/EmojiArtDocumentView.swift
@@ -7,12 +7,59 @@
 
 import SwiftUI
 
+// View
 struct EmojiArtDocumentView: View {
+    
+    @ObservedObject var document: EmojiArtDocument
+    private let emojis = "👻🍎😃🤪☹️🤯🐶🐭🦁🐵🦆🐝🐢🐄🐖🌲🌴🌵🍄🌞🌏🔥🌈🌧️🌨️☁️⛄️⛳️🚗🚙🚓🚲🛺🏍️🚘✈️🛩️🚀🚁🏰🏠❤️💤⛵️"
+    private let paletteEmojiSize: CGFloat = 40
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            documentBody
+            ScrollingEmojis(emojis)
+                .font(.system(size: paletteEmojiSize))
+                .padding(.horizontal)
+                .scrollIndicators(.hidden)
+        }
+    }
+    
+    private var documentBody: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Color.white
+                // image goes here
+                ForEach(document.emojis) { emoji in
+                    Text(emoji.string)
+                        .font(emoji.font)
+                        .position(emoji.position.in(geometry))
+                }
+            }
+        }
+    }
+}
+
+struct ScrollingEmojis: View {
+    
+    let emojis: [String]
+    
+    init(_ emojis: String) {
+        // take a character and convert it to String
+//        self.emojis = emojis.uniqued.map { String($0) }
+        self.emojis = emojis.uniqued.map(String.init)
+    }
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach (emojis, id: \.self ) { emoji in
+                    Text(emoji)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    EmojiArtDocumentView()
+    EmojiArtDocumentView(document: EmojiArtDocument())
 }
