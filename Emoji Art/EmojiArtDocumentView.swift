ae4ce95205bbb941be43a84b9a831f23b2c7c5d6
Cephei.DZ <Buster2491@gmail.com>
2024-05-23 16:07:41 +0800

    Init codes

---
+++ Emoji Art/EmojiArtDocumentView.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiDucomentView.swift
+//  Emoji Art
+//
+//  Created by Vic.Z Ding on 2024/4/22.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmojiArtDocumentView()
+}
---
+++ Emoji Art/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  Emoji Art
+//
+//  Created by Vic.Z Ding on 2024/4/22.
+//
+
+import Foundation
